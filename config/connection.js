0c6c4a27808d623a1d95208af79aa2ac7606db83
imalam64 <imalam64@gmail.com>
2018-09-06 21:58:59 -0700

    set up directory

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  //database: "cat_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
